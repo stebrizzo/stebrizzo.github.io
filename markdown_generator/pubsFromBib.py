c49536f36ac15514411b4cc6a16b04cc68d1a326
Stefano Brizzolara <brizzo_94@hotmail.it>
2026-09-12 02:04:15 -0400

    Update pubsFromBib.py

--- markdown_generator/pubsFromBib.py
+++ markdown_generator/pubsFromBib.py
@@ -27,20 +27,26 @@ import re
 #todo: incorporate different collection types rather than a catch all publications, requires other changes to template
 publist = {
     "proceeding": {
-        "file" : "proceedings.bib",
+        "file": "proceedings.bib",
         "venuekey": "booktitle",
         "venue-pretext": "In the proceedings of ",
-        "collection" : {"name":"publications",
-                        "permalink":"/publication/"}
-        
+        "category": "conferences",
+        "collection": {
+            "name": "publications",
+            "permalink": "/publication/"
+        }
     },
-    "journal":{
+
+    "journal": {
         "file": "pubs.bib",
-        "venuekey" : "journal",
-        "venue-pretext" : "",
-        "collection" : {"name":"publications",
-                        "permalink":"/publication/"}
-    } 
+        "venuekey": "journal",
+        "venue-pretext": "",
+        "category": "manuscripts",
+        "collection": {
+            "name": "publications",
+            "permalink": "/publication/"
+        }
+    }
 }
 
 html_escape_table = {
